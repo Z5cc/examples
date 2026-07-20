a7877b134b98e269b713abc26d2f473761c6a392
david <d.ncklsr@gmail.com>
2026-07-20 21:40:49 +0200

    neural network

--- example.py
+++ example.py
@@ -67,19 +67,50 @@ class Data:
 class Model:
     def __init__(self):
         # self.model = LinearRegression()
-        self.model = make_pipeline(PolynomialFeatures(degree=4, include_bias=False),LinearRegression())
+        # self.model = make_pipeline(PolynomialFeatures(degree=4, include_bias=False),LinearRegression())
         # self.model = DecisionTreeRegressor()
+        self.model = NeuralNetwork()
         # neural network
         # increase dimensions of neural network until dimension of my 3d bbox problem. and for 3d bbox problem also try trees and polynomials.
         # increase size and dimension of problem until i can see that neural network helps over tree or polynomial regression
         # get intuition about inductive bias of CNN and Transformers
 
     def train(self, X, y):
-        self.model.fit(X, y)
+        if isinstance(self.model, nn.Module):
+            self.train_nn(X, y)
+        else:
+            self.model.fit(X, y)
+
+    def train_nn(self, X, y):
+        X = torch.from_numpy(X).type(torch.float32)
+        y = torch.from_numpy(y).type(torch.float32)
+        optimizer = torch.optim.SGD(self.model.parameters())
+        criterion = nn.MSELoss()
+        for _ in range(1000):
+            # forward
+            y_pred= self.model(X)
+            loss = criterion(y_pred, y)
+            # backwards
+            loss.backward()
+            # update weights according to gradients of weights from backward pass
+            optimizer.step()
+            optimizer.zero_grad()
+
+
 
     def predict(self, X):
-        y_pred = self.model.predict(X)
+        if isinstance(self.model, nn.Module):
+            y_pred = self.predict_nn(X)
+        else:
+            y_pred = self.model.predict(X)
         return y_pred
+    
+    def predict_nn(self, X):
+        X = torch.from_numpy(X).type(torch.float32)
+        y_pred = self.model(X)
+        y_pred = y_pred.detach().numpy()
+        return y_pred
+        
     
 
 
@@ -89,13 +120,15 @@ class NeuralNetwork(nn.Module):
     def __init__(self):
         super().__init__()
         self.lin1 = nn.Linear(2,10)
-        self.lin2 = nn.Linear(10,10)
-        self.lin3 = nn.Linear(10,1)
+        self.lin2 = nn.Linear(10,50)
+        self.lin3 = nn.Linear(50,50)
+        self.lin4 = nn.Linear(50,1)
 
     def forward(self, x):
         x = F.relu(self.lin1(x)) # hidden layer 1
         x = F.relu(self.lin2(x)) # hidden layer 2
-        x = self.lin3(x)         # output layer
+        x = F.relu(self.lin3(x)) # hidden layer 3
+        x = self.lin4(x)         # output layer
         return x
 
 
